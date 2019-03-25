7d85fa1c4de1da00bd6e40918137a0cf07e6f016
joseck ogachi <jogachi4@gmail.com>
2019-03-26 02:48:03 +0300

    add changes to views.py

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
+def display(request):
+    images = Profile.objects.all()
+    try:
+        hoodpro_id = get_object_or_404(hoodpro,pk=request.user.profile.hoodpro.id)
+
+        photo = hoodpro.objects.get(pk=request.user.profile.location.id)
+        photos = Post.objects.filter(hoodpro=photo)
+        post = hoodpro.objects.get(pk=request.user.profile.hoodpro.id)
+        posts = Post.objects.filter(hoodpro=post)
+    except:
+        message='create neighbourhood'
+    return render (request,'home.html',locals())
