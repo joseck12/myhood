1b83ea1a5f738bcdf7c682e5271ffc9752785a40
joseck ogachi <jogachi4@gmail.com>
2019-03-26 02:56:48 +0300

    add modifications to views.py

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -21,3 +21,18 @@ def create_profile_view(request):
             form = ProfileForm(request.POST,request.FILES,instance=current_user.profile)
         if form.is_valid():
            form.save()
+        else:
+        form =ProfileForm()
+    return render(request,'profile/profile.html',{"form":form,"profile":profile})
+def create_post_view(request):
+    current_user = request.user
+    # post = Post.objects.filter(hoodpro=request.user.profile.hoodpro.id)
+    if request.method == 'POST':
+                post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.user = current_user
+            post.profile = current_user.profile
+            hoodpro = current_user.profile.hoodpro
+            post.save()
+        return redirect('display')
