50e9a1d7a3db35ca646039d2f7b643325e3f100e
joseck ogachi <jogachi4@gmail.com>
2019-03-26 02:56:29 +0300

    add modifications to urls.py

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns=[
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'profile/$',views.create_profile_view,name='profile'),
+    url(r'^$', views.display, name='display'),
+    url(r'^post/$', views.create_post_view, name='post'),
 
 ]
