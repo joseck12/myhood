3eda42566b0215191dfc01fd8a812a1226fdef54
joseck ogachi <jogachi4@gmail.com>
2019-03-26 03:06:06 +0300

    add modifications to urls.py

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -9,5 +9,7 @@ urlpatterns=[
     url(r'profile/$',views.create_profile_view,name='profile'),
     url(r'^$', views.display, name='display'),
     url(r'^post/$', views.create_post_view, name='post'),
-
+    url(r'^bizz/$', views.create_buisiness_view, name='bizz'),
+    url(r'^business/$', views.business, name='business'),
+    url(r'^add/$', views.create_community, name='community'),
 ]
