264568db4645b5e73b2760a8144ebfb635824886
joseck ogachi <jogachi4@gmail.com>
2019-03-26 03:30:55 +0300

    add changes to admin.py

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile,Post,hoodpro,Location
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(hoodpro)
+admin.site.register(Location)
