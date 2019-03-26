786d204c4092c66545210b70809c994687aeac38
joseck ogachi <jogachi4@gmail.com>
2019-03-26 03:27:43 +0300

    add content to urls.py

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url,include
 from . import views
-
-
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url(r'^accounts/', include('registration.backends.simple.urls')),
@@ -13,5 +13,7 @@ urlpatterns=[
     url(r'^business/$', views.business, name='business'),
     url(r'^add/$', views.create_community, name='community'),
     url(r'^search/',views.search_results, name='search_results'),
-
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
