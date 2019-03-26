18f513fe9143c492a667513ac2cf58ccb7c613f1
joseck ogachi <jogachi4@gmail.com>
2019-03-26 03:07:02 +0300

    add content to urls.py

--- hoodpro/urls.py
+++ hoodpro/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
